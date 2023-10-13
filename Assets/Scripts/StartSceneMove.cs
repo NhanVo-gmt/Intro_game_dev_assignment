192b4935ed7db423673d6b59880207265fe805c4
Thanh Nhan <141220487+NhanVoo@users.noreply.github.com>
2023-10-13 20:14:13 +1100

    customize startscene

--- Assets/Scripts/StartSceneMove.cs
+++ Assets/Scripts/StartSceneMove.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class StartSceneMove : MonoBehaviour
 {
     [SerializeField] private List<Vector2> moveDirection = new List<Vector2>();
+    [SerializeField] private int moveStep = 21;
+    [SerializeField] private bool isVisible = true;
 
     private Player player;
+    private SpriteRenderer sprite;
 
     private void Awake()
     {
         player = GetComponent<Player>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
@@ -21,12 +25,12 @@ public class StartSceneMove : MonoBehaviour
 
     void AddMoveDirection()
     {
-        for (int i = 0; i < 12; i++)
+        for (int i = 0; i < moveStep; i++)
         {
             moveDirection.Add(Vector2.left);
         }
 
-        for (int i = 0; i < 12; i++)
+        for (int i = 0; i < moveStep; i++)
         {
             moveDirection.Add(Vector2.right);
         }
@@ -36,6 +40,17 @@ public class StartSceneMove : MonoBehaviour
     {
         for (int i = 0; i < moveDirection.Count; i++)
         {
+            if (!isVisible)
+            {
+                if (i == moveStep - 1)
+                {
+                    sprite.enabled = true;
+                }
+                else if (i == 0)
+                {
+                    sprite.enabled = false;
+                }
+            }
             yield return player.MoveCoroutine(moveDirection[i]);
         }
 
